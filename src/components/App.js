b28f0d95aac8eb2a6523c1f23482c4249ebd1272
NatalyOC <noteroc@hotmail.com>
2018-03-26 12:23:43 -0500

    modificando componente login

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import Login from './Login'
 const App = () => (
   <div>
-  <h1 >App del clima</h1>
+    <h1 >App del clima</h1>
+    <Login/>
 </div>
 )
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,19 @@
 import React from 'react';
 
+const Login =({signInGoogle})=>{
 
 
-export default Login;
+  return (
+    <a href="" className="btn btn-light" onClick={signInGoogle}>Login with Google</a>
+  )
+}
+const mapStateToProps=(state)=>({
+
+})
+
+const mapDispatchToProps=(dispatch)=>({
+
+})
+
+export default connect(mapStateToProps,mapDispatchToProps) (Login);
 
