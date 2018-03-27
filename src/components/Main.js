2b4cd7d44af7e2e4497054bfb0acf5735fbf1e88
Claudiagari <claudia.garibotto@gmail.com>
2018-03-27 12:57:34 -0500

    añadiendo cambios de funcionalidad

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, {Component} from 'react';
 import fetch from 'fetch-with-proxy';
 
 class Main extends React.Component {
-
-  constructor(props) {
+    constructor(props) {
     super(props)
     this.state = { temperatura: '',
                     humedad: '' ,
@@ -11,19 +10,19 @@ class Main extends React.Component {
                   }
   }
   componentWillMount() {
-    var lat, lng;
-    navigator.geolocation.getCurrentPosition(handle_geolocation_query);  
-    function handle_geolocation_query(position){  
-        lat = position.coords.latitude;    
-        lng =  position.coords.longitude; 
+    navigator.geolocation.getCurrentPosition(
+    function (position){  
+        let lat = position.coords.latitude;    
+        let lng =  position.coords.longitude; 
         console.log(lat)
-    } 
-        const url = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/ef053d30e83f21037db223112b6728ed/-12.168251699999999,-77.0046148?lang=es'
-    fetch(url)
+      })
+        /*const url = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/ef053d30e83f21037db223112b6728ed/-12.168251699999999,-77.0046148?lang=es'*/
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/ef053d30e83f21037db223112b6728ed/-12.168251699999999,-77.0046148?lang=es`)
       .then((response) => {
         return response.json()
       })
       .then((data) => {
+        console.log
         this.setState({ temperatura: data.currently.temperature,
                          humedad : data.currently.humidity, 
                          Uv: data.currently.uvIndex                                 
@@ -36,6 +35,8 @@ class Main extends React.Component {
         <div className="container">
           <div  />
           <p>{((this.state.temperatura- 32) * 5 / 9).toFixed(2)}<span>°</span></p>
+          <p>{(this.state.humedad)*100}<span>%</span></p>
+          <p>{(this.state.Uv)}<span></span></p>
         </div>
       )
   }
--- src/components/Select-bar.js
+++ src/components/Select-bar.js
@@ -1,11 +1,17 @@
 import React from 'react'
-const SelectBar =()=>(
-  <select className="custom-select">
-  <option selected>Open this select menu</option>
-  <option value="1">One</option>
-  <option value="2">Two</option>
-  <option value="3">Three</option>
-</select>
+const SelectBar = () => (
+  <div className="container text-center">
+  <div className="row text-center">
+  <div className=" col-12 col-md-6 offset-md-3 div-select">
+    <h5>¿Cómo sientes hoy el clima?</h5>
+    <select className="custom-select">
+      <option selected>Selecciona una opción</option>
+      <option value="1"></option>
+      <option value="2"></option>
+      <option value="3"></option>
+    </select>
+    </div>
+    </div>
+  </div>
 )
-
 export default SelectBar
