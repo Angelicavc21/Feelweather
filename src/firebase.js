062b0f49879a668ad2884d9f71430c38c2032bb9
NatalyOC <noteroc@hotmail.com>
2018-03-26 23:10:41 -0500

    implementando firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyBzkyqmjw_BvRHMShdmwQigLeeoX_OJeLM",
+  authDomain: "app-clima-1dcd7.firebaseapp.com",
+  databaseURL: "https://app-clima-1dcd7.firebaseio.com",
+  projectId: "app-clima-1dcd7",
+  storageBucket: "app-clima-1dcd7.appspot.com",
+  messagingSenderId: "431370027388"
+};
+
+firebase.initializeApp(config);
+const googleAuthProvider = new firebase.auth.googleAuthProvider();
+
+export {firebase, googleAuthProvider};
