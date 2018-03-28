8240a08d500391678d9e548549e7e627c5eb54f4
NatalyOC <noteroc@hotmail.com>
2018-03-28 03:03:36 -0500

    modificando estilos Signin

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,15 @@ import logoblancocorto from '../assets/brand/logoblancocorto.png'
 
 const Header =({user,onLogin,startLogout})=>(
   <header className="navbar navbar-expand flex-column flex-md-row">
-    <nav className="nav navbar-expand">
+    <nav className="navbar navbar-expand ">
       <a className="navbar-brand mr-0 mr-md-2" href="#">
         <img src={logoblancocorto} width="50" height="50" className="d-inline-block align-top" alt="logo"/>
       </a>
-      <form className="form-inline mr-auto form-search">
+      <form className="form-inline mr-auto form-search flex-row">
         <input className="form-control search" type="search" placeholder="Search" aria-label="Search"/>
         <button className="btn btn-search" type="submit"><i className="fas fa-search"></i></button>
       </form>
-      <ul className="navbar-nav flex-row ml-md-auto d-none d-md-flex my-2 my-md-0">
+      <ul className="navbar-nav flex-row ml-md-auto d-none d-md-flex ">
         <li className="nav-item"><img width="32" className="rounded-circle"src={user.photoURL}/></li>
         <li className="nav-item">{user.displayName}</li>
       </ul>
